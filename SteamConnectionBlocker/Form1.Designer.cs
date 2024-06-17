bcdd23e56b5dd951153794233cfd5799a676306e
Nightfall <greennightfall@protonmail.com>
2024-06-17 23:13:37 +0200

    first

---
+++ SteamConnectionBlocker/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace SteamConnectionBlocker
+{
+    partial class Mainform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Mainform));
+            this.switch_blockConnection = new MaterialSkin.Controls.MaterialSwitch();
+            this.btn_about = new MaterialSkin.Controls.MaterialButton();
+            this.SuspendLayout();
+            // 
+            // switch_blockConnection
+            // 
+            this.switch_blockConnection.AutoSize = true;
+            this.switch_blockConnection.Depth = 0;
+            this.switch_blockConnection.Location = new System.Drawing.Point(44, 145);
+            this.switch_blockConnection.Margin = new System.Windows.Forms.Padding(0);
+            this.switch_blockConnection.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.switch_blockConnection.MouseState = MaterialSkin.MouseState.HOVER;
+            this.switch_blockConnection.Name = "switch_blockConnection";
+            this.switch_blockConnection.Ripple = true;
+            this.switch_blockConnection.Size = new System.Drawing.Size(181, 37);
+            this.switch_blockConnection.TabIndex = 0;
+            this.switch_blockConnection.Text = "Block Connection";
+            this.switch_blockConnection.UseVisualStyleBackColor = true;
+            this.switch_blockConnection.CheckedChanged += new System.EventHandler(this.switch_blockConnection_CheckedChanged);
+            // 
+            // btn_about
+            // 
+            this.btn_about.AutoSize = false;
+            this.btn_about.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btn_about.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.btn_about.Depth = 0;
+            this.btn_about.HighEmphasis = true;
+            this.btn_about.Icon = null;
+            this.btn_about.Location = new System.Drawing.Point(205, 241);
+            this.btn_about.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btn_about.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btn_about.Name = "btn_about";
+            this.btn_about.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.btn_about.Size = new System.Drawing.Size(70, 28);
+            this.btn_about.TabIndex = 1;
+            this.btn_about.Text = "About";
+            this.btn_about.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.btn_about.UseAccentColor = false;
+            this.btn_about.UseVisualStyleBackColor = true;
+            this.btn_about.Click += new System.EventHandler(this.btn_about_Click);
+            // 
+            // Mainform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 278);
+            this.Controls.Add(this.btn_about);
+            this.Controls.Add(this.switch_blockConnection);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Mainform";
+            this.Sizable = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Steam Connection Blocker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialSwitch switch_blockConnection;
+        private MaterialSkin.Controls.MaterialButton btn_about;
+    }
+}
+
---
+++ SteamConnectionBlocker/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace SteamConnectionBlocker
+{
+    public partial class Mainform : MaterialForm
+    {
+        public bool isConnectionBlocked = false;
+        public Mainform()
+        {
+            InitializeComponent();
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue100, TextShade.WHITE);
+        }
+
+        private async void switch_blockConnection_CheckedChanged(object sender, System.EventArgs e)
+        {
+            isConnectionBlocked = !isConnectionBlocked;
+            await myFuncs.blockConnection(isConnectionBlocked);
+            
+        }
+
+        private void btn_about_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Made by NightfallCT");
+        }
+    }
+
+    public class myFuncs
+    {
+        public static async Task blockConnection(bool isBlocked)
+        {
+            string tempFile = Path.GetTempFileName() + ".bat";
+
+            if (isBlocked)
+            {
+                string blockCmd = @"netsh advfirewall firewall add rule name=""_02 Block steam(Program)"" dir=in program=""C:\Program Files (x86)\Steam\bin\cef\cef.win7x64\steamwebhelper.exe"" action=block
+netsh advfirewall firewall add rule name=""_01 Block steam(Program)"" dir=in program=""C:\Program Files (x86)\Steam\steam.exe"" action=block
+netsh advfirewall firewall add rule name=""_02 Block steam(Program)"" dir=out program=""C:\Program Files (x86)\Steam\bin\cef\cef.win7x64\steamwebhelper.exe"" action=block
+netsh advfirewall firewall add rule name=""_01 Block steam(Program)"" dir=out program=""C:\Program Files (x86)\Steam\steam.exe"" action=block";
+                File.WriteAllText(tempFile, blockCmd);
+            }
+            else
+            {
+                string freeCmd = @"netsh advfirewall firewall set rule name=\""_01 Block steam(Program)\"" new enable=no
+netsh advfirewall firewall set rule name=\""_02 Block steam(Program)\"" new enable=no
+netsh advfirewall firewall delete rule name=""_01 Block steam(Program)
+netsh advfirewall firewall delete rule name=""_02 Block steam(Program)""";
+
+                File.WriteAllText(tempFile, freeCmd);
+            }
+
+            var process = new Process();
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = tempFile,
+                WindowStyle = ProcessWindowStyle.Hidden,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false
+            };
+            process.StartInfo = startInfo;
+            process.Start();
+
+            // Handle output and error streams
+            await Task.WhenAll(
+                Task.Run(() => process.StandardOutput.ReadToEndAsync()),
+                Task.Run(() => process.StandardError.ReadToEndAsync()),
+                Task.Run(() => process.WaitForExit())
+            );
+
+            File.Delete(tempFile);
+        }
+
+    }
+}
